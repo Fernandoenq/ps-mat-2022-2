741b8a9699b0ae5de0b4929457dca791f433641a
Fernando <fernandoasfilho74@gmail.com>
2022-12-02 10:53:14 -0300

    Voltando a minha versão dos alunos e app.js (não adiantou))

--- models/aluno.js
+++ models/aluno.js
@@ -4,96 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Aluno extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      this.belongsTo(models.Turma, {
-        foreignKey: 'turma_id',
-        targetKey: 'id',
-        as: 'turma'
-      })
-    }
-  }
-  Aluno.init({
-    // Model attributes are defined here
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    nome: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-    },
-    data_nascimento: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
-    },
-    doc_identidade: {
-        type: DataTypes.STRING(20),
-        allowNull: false
-    },
-    cpf: {
-        type: DataTypes.CHAR(14),
-        allowNull: false
-    },
-    logradouro: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-    },
-    num_imovel: {
-        type: DataTypes.STRING(10),
-        allowNull: false
-    },
-    complemento: {
-        type: DataTypes.STRING(50),
-        allowNull: true
-    },
-    bairro: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-    },
-    municipio: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-    },
-    uf: {
-        type: DataTypes.CHAR(2),
-        allowNull: false
-    },
-    telefone: {
-        type: DataTypes.STRING(20),
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-    }
-  }, {
-    sequelize,
-    modelName: 'Aluno',
-    tableName: 'alunos'
-  });
-  return Aluno;
-};
-
-
-
-
-/*
-
-
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Aluno extends Model {
     
      //* Helper method for defining associations.
      //* This method is not a part of Sequelize lifecycle.
@@ -173,4 +83,3 @@ module.exports = (sequelize, DataTypes) => {
 };
 
 
-*/
--- routes/aluno.js
+++ routes/aluno.js
@@ -3,23 +3,6 @@ const router = express.Router()
 const controller = require('../controllers/aluno')
 const verifToken = require('../lib/verif_token')
 
-router.post('/', verifToken, controller.create)
-router.get('/', verifToken, controller.retrieve)
-// :id é uma parte variável da URI que será interpretada
-// como um parâmetro chamado id
-router.get('/:id', verifToken, controller.retrieveOne)
-router.patch('/:id', verifToken, controller.update)
-router.delete('/:id', verifToken, controller.delete)
-
-module.exports = router
-
-
-
-/*const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/aluno')
-const verifToken = require('../lib/verif_token')
-
 router.post('/', verifToken,  controller.create)
 router.get('/', verifToken,  controller.retrieve)
 // :id é uma parte variável da URI que será interpretada
@@ -30,4 +13,3 @@ router.delete('/:id', verifToken, controller.delete)
 
 module.exports = router
 
-*/
