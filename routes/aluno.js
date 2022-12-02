c2415bed51490e65a8973ae7f686ada82c24e35b
Fernando <fernandoasfilho74@gmail.com>
2022-12-02 10:06:58 -0300

    Back End 500 error (alterado app.js e alunos do cont, models e routes ) (teste)
    erro ao consultar a tabela alunos
    porem o login e token funcionam

--- routes/aluno.js
+++ routes/aluno.js
@@ -3,6 +3,23 @@ const router = express.Router()
 const controller = require('../controllers/aluno')
 const verifToken = require('../lib/verif_token')
 
+router.post('/', verifToken, controller.create)
+router.get('/', verifToken, controller.retrieve)
+// :id é uma parte variável da URI que será interpretada
+// como um parâmetro chamado id
+router.get('/:id', verifToken, controller.retrieveOne)
+router.patch('/:id', verifToken, controller.update)
+router.delete('/:id', verifToken, controller.delete)
+
+module.exports = router
+
+
+
+/*const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/aluno')
+const verifToken = require('../lib/verif_token')
+
 router.post('/', verifToken,  controller.create)
 router.get('/', verifToken,  controller.retrieve)
 // :id é uma parte variável da URI que será interpretada
@@ -12,3 +29,5 @@ router.patch('/:id', verifToken,  controller.update)
 router.delete('/:id', verifToken, controller.delete)
 
 module.exports = router
+
+*/
